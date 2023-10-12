e9c44dfc3e0fcaa497f6f33edac8737f9e75f8ef
Oliver Cieplinski <olivercieplinski@gmail.com>
2023-10-12 15:57:01 +0100

    Created Info Page Overlay

--- js/infobar.js
+++ js/infobar.js
@@ -1,14 +1,32 @@
 const options = document.querySelectorAll(".option");
-const textboxes = document.querySelectorAll(".textbox");
+const textbox = document.querySelector(".textbox");
+let activeOption = null;
 
 options.forEach((option, index) => {
     option.addEventListener("click", () => {
-        // Hide all textboxes
-        textboxes.forEach((textbox) => {
+        // Hide the textbox
+        setTimeout(() => {
             textbox.style.opacity = 0;
-        });
+        }, 0);
+        if (activeOption) {
+            activeOption.classList.remove("active");
+        }
+        // Set the unique text for the selected option
+        const texts = [
+            "This is the content for Option 1.",
+            "This is the content for Option 2.",
+            "This is the content for Option 3.",
+            "This is the content for Option 4."
+        ];
 
-        // Show the selected textbox with fading effect
-        textboxes[index].style.opacity = 1;
+        // Show the selected text
+        setTimeout(() => {
+            textbox.innerHTML = texts[index];
+            textbox.style.opacity = 1;
+        }, 500);
+        option.classList.add("active");
+
+        // Store the active option for future reference
+        activeOption = option;
     });
 });
